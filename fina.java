336456e0c267087103a789f4ae2216170a97dae1
alexmntmnk <114926353+alexmntmnk@users.noreply.github.com>
2025-10-14 16:04:24 +0300

    Update fina.java

--- fina.java
+++ fina.java
@@ -1,13 +1,15 @@
 @Query("""
-    SELECT sit.objectId
-    FROM SendToEgaisDb sit
-    WHERE sit.documentDateCreate >= :startDate
-    GROUP BY sit.objectId
-    HAVING COUNT(CASE WHEN sit.logStatus = :statusSent THEN 1 END) = 1
-       AND COUNT(CASE WHEN sit.logStatus = :statusReceived THEN 1 END) = 0
+    SELECT count(a.objectId) FROM (
+        SELECT slt.objectId
+        FROM SendLogToAbed slt
+        WHERE slt.documentDateCreate >= :startDate
+        GROUP BY slt.objectId
+        HAVING COUNT(CASE WHEN slt.logStatus = :statusSent THEN 1 END) = 1
+           AND COUNT(CASE WHEN slt.logStatus = :statusReceived THEN 1 END) = 0
+    ) a
 """)
-List<String> findObjectIdsWithOneSentAndNoReceived(
-    @Param("statusSent") SendingToAbsdStatus statusSent,
-    @Param("statusReceived") SendingToAbsdStatus statusReceived,
+Long countDocumentWithExactlyOneSendAndNoReceive(
+    @Param("statusSent") SendLogToAbedStatus statusSent,
+    @Param("statusReceived") SendLogToAbedStatus statusReceived,
     @Param("startDate") LocalDateTime startDate
 );
